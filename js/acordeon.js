37efbf1c89ec50e7cf2677af2aff4cbc7225d80d
Donchenko Eugine <donchenkoeugine@gmail.com>
2017-06-20 22:27:25 +0300

    Added property "oneOnlyOpen"

--- js/acordeon.js
+++ js/acordeon.js
@@ -3,7 +3,7 @@
 	let acordeonParts = acordeon.querySelectorAll(".part_acordeon");
 	let allTitleItems = acordeon.querySelectorAll(".title_item_acordeon");
 	let amountOpen = 0;
-	let repository = {	oneAlwaysOpen	: false,
+	let repository = {	oneAlwaysOpen	: true,
 						oneOnlyOpen		: true,
 						items			: []};
 	for (let item of acordeon.querySelectorAll(".item_acordeon")) {
@@ -18,16 +18,29 @@
 		amountOpen++;
 	}
 
+	function controlOpenItems(elem) {
+		for (let i=0; i<repository.items.length; i++) {
+			if (acordeonParts[i].classList.contains("open_acordeon") && !elem.classList.contains("selected_item_acordeon")) {
+				acordeonParts[i].classList.remove("open_acordeon");
+				acordeonParts[i].parentElement.querySelector(".title_item_acordeon").classList.remove("selected_item_acordeon");
+				amountOpen--;
+			}
+		}
+	}
+
 	acordeon.addEventListener("click", event=>{
 		let target = event.target;
+		let partTarget = target.parentElement.querySelector(".part_acordeon");
 		if (repository.oneAlwaysOpen && amountOpen===1 && target.classList.contains("selected_item_acordeon")) {
 			return;
-		} 
-		
+		}
+		if (repository.oneOnlyOpen && amountOpen>0) {
+			controlOpenItems(target);
+		}
 		if (target.classList.contains("title_item_acordeon")) {
-			target.parentElement.querySelector(".part_acordeon").classList.toggle("open_acordeon");
+			partTarget.classList.toggle("open_acordeon");
 			target.classList.toggle("selected_item_acordeon");
-			if (target.classList.contains("selected_item_acordeon")) {
+			if (partTarget.classList.contains("open_acordeon")) {
 				amountOpen++;
 			} else {
 				amountOpen--;
